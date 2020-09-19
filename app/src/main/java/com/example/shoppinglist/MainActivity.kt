8685c9b43dc5a7c5380adf8e61a3972fe4566b12
Mikael Enghaug <Mikael093@hotmail.com>
2020-09-19 19:54:00 +0200

    Add all files

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.shoppinglist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import com.example.shoppinglist.SecondActivity as SecondActivity1
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        //Navigate button to SecondActivity
+        next_button.setOnClickListener {
+            startActivity(Intent(this, com.example.shoppinglist.SecondActivity:: class.java))
+        }
+
+    }
 }
